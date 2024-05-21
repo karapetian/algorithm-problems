0702867e48e0ba01226cfef89b86b3ac8148958c
Varduhi Karapetyan <varduhi.karapetian@gmail.com>
2024-05-21 09:49:29 +0400

    Merge 2 Sorted Arrays

---
+++ src/main/java/org/example/algo/practicum/yandex/merge_sorted_arrays/Solution.java
@@ -0,0 +1,37 @@
+package org.example.algo.practicum.yandex.merge_sorted_arrays;
+
+//Merge 2 Sorted Arrays
+// O(n+m)
+public class Solution {
+
+    static int[] merge(int[] input1, int[] input2) {
+        int resultLenght = input1.length + input2.length;
+        int[] result = new int[resultLenght];
+
+        int input_1_pointer = 0;
+        int input_2_pointer = 0;
+        int i = 0;
+
+        for ( ;
+             0 < resultLenght && (input_1_pointer != input1.length && input_2_pointer != input2.length);
+             i++) {
+            if (input1[input_1_pointer] < input2[input_2_pointer]) {
+                result[i] = input1[input_1_pointer];
+                input_1_pointer++;
+            } else {
+                result[i] = input2[input_2_pointer];
+                input_2_pointer++;
+            }
+        }
+
+        while (input_1_pointer < input1.length) {
+            result[i++] = input1[input_1_pointer++];
+        }
+
+        while (input_2_pointer < input2.length) {
+            result[i++] = input2[input_2_pointer++];
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/org/example/algo/practicum/yandex/merge_sorted_arrays/Test.java
@@ -0,0 +1,13 @@
+package org.example.algo.practicum.yandex.merge_sorted_arrays;
+
+import java.util.Arrays;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] input1 = new int[]{3, 5, 7, 8};
+        int[] input2 = new int[]{1, 2, 4, 6};
+        int[] ints = Solution.merge(input1, input2);
+        System.out.println(Arrays.toString(ints));
+    }
+}
