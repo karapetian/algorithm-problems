d22a3c32769d70d08c048e659fc3911b245fc3f5
Varduhi Karapetyan <varduhi.karapetian@gmail.com>
2025-02-06 10:17:59 +0400

    283. Move Zeroes

--- src/main/java/org/example/algo/yandex/move_zeroes/Solution.java
+++ src/main/java/org/example/algo/yandex/move_zeroes/Solution.java
@@ -34,7 +34,38 @@ public class Solution {
         }
     }
 
-    // {0,1,0,3,12}
+    //{0,1,0,3,12}
+    //{5, 4, 0,1,0,3,12}
+    //{5, 4, 1,0,0,3,12}
+    static void moveZeroes2(int[] nums) {
+        int pointer1 = 0;
+        int pointer2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[pointer1] != 0 && nums[pointer2] != 0) {
+                pointer1++;
+                pointer2++;
+                continue;
+            }
+
+            if (nums[pointer2] == 0) {
+                do {
+                    pointer2++;
+                } while (nums[pointer2] != 0);
+
+                //swap
+                int tmp = nums[pointer1];
+                nums[pointer1] = nums[pointer2];
+                nums[pointer2] = tmp;
+
+                pointer1++;
+                pointer2++;
+            }
+        }
+    }
+
+    // {4, 0,1,0,3,12}
     static void moveZeroes3(int[] nums) {
         int lastNonZeroFoundAt = 0;
         for (int cur = 0; cur < nums.length; cur++) {
@@ -46,5 +77,38 @@ public class Solution {
                 lastNonZeroFoundAt++;
             }
         }
+    }
+
+
+    // {4,5,0,1,0,3,12}
+    // {4, 0, 3, 5}
+    static void moveZeroes4(int[] nums) {
+        if (nums.length==1) {
+            return;
+        }
+        int left = 0; //to find zeroes
+        int right = 1; //to find non-zeroes
+
+        while (right < nums.length) {
+            if (nums[left] != 0) {
+                left++;
+                right++;
+                continue;
+            }
+            if (nums[right] == 0) {
+                right++;
+                continue;
+            }
+            if (nums[right] != 0 && nums[left] == 0) {
+                //swap
+                int tmp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = tmp;
+
+                right++;
+                left++;
+            }
+        }
+
     }
 }
--- src/main/java/org/example/algo/yandex/move_zeroes/Test.java
+++ src/main/java/org/example/algo/yandex/move_zeroes/Test.java
@@ -3,13 +3,14 @@ package org.example.algo.yandex.move_zeroes;
 import java.util.Arrays;
 
 import static org.example.algo.yandex.move_zeroes.Solution.moveZeroes3;
+import static org.example.algo.yandex.move_zeroes.Solution.moveZeroes4;
 
 public class Test {
 
     public static void main(String[] args) {
-        int [] nums = {4,0,1,0,3,12};
+        int [] nums = {4,5,0,1,0,3,12};
 
-        moveZeroes3(nums);
+        moveZeroes4(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
