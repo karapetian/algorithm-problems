713c7630cc14da716255ea75df55bed2f299fdee
Varduhi Karapetyan <varduhi.karapetian@gmail.com>
2025-05-19 23:51:29 +0400

    198. House Robber

---
+++ src/main/java/org/example/algo/yandex/array/house_robber/Solution.java
@@ -0,0 +1,30 @@
+package org.example.algo.yandex.array.house_robber;
+
+//198. House Robber
+//https://www.youtube.com/watch?v=CCuHIYK04jE&t=171s
+public class Solution {
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] stepResults = new int[nums.length]; //an array that will keep the max result of the each step
+        stepResults[0] = nums[0];
+        stepResults[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i ++) {
+            stepResults[i] = Math.max(stepResults[i-1], (nums[i]+stepResults[i-2]));
+        }
+
+        return stepResults[nums.length-1];
+    }
+}
---
+++ src/main/java/org/example/algo/yandex/array/house_robber/Test.java
@@ -0,0 +1,4 @@
+package org.example.algo.yandex.array.house_robber;
+
+public class Test {
+}
